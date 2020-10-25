65bd865b204166c0839a857b4caf837a4684d403
bnulo <55716349+bnulo@users.noreply.github.com>
2020-10-26 01:04:48 +0330

    Update singleton2.swift

--- singleton2.swift
+++ singleton2.swift
@@ -2,16 +2,16 @@ import Foundation
 // The singleton pattern guarantees that only one instance of a class is instantiated
 // global access
 //static property and private initializer
-class NetworkManager2 {
-    let baseUrl: String
-    static let shared = NetworkManager2(baseUrl: "baseURL")
-    private init(baseUrl: String) {
-        self.baseUrl = baseUrl
+class A {
+    let property: String
+    static let shared = A(property: "some text")
+    private init(property: String) {
+        self.property = property
     }
 }
 
-class Presenter2 {
-    func request() {
-        print(NetworkManager2.shared)
+class B {
+    func someFunc() {
+        print(A.shared)
     }
 }
